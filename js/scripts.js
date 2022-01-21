787c583d9eeddb795899c30091989e111650076d
Chase Stokes <chasehunterstokes@gmail.com>
2022-01-21 14:50:32 -0800

    add further styling and fix typos in HTML and in UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,13 @@ function beepBoop(input, name) {
   for (i = 0; i <= input; i++) {
     let iString = i.toString();
     if (iString.includes("3")) {
-      outputArray.push(name + ", wont you be my neighbor")
-    } else if (iString.includes("2")) {
-      outputArray.push("boop");
-    } else if (iString.includes("1")) {
-      outputArray.push("beep");
+      outputArray.push("" +name + ", wont you be my neighbor ")
+    } else if (iString.includes(" 2")) {
+      outputArray.push(" boop ");
+    } else if (iString.includes(" 1")) {
+      outputArray.push(" beep");
     } else {
-      outputArray.push(i);
+      outputArray.push(" " + i);
     }
   }
   return outputArray
@@ -19,8 +19,9 @@ $(document).ready(function(){
   $("#formOne").submit(function(event){
     event.preventDefault();
     let input = $("#number").val();
-    let name = ("#name").val();
+    let name = $("#name").val();
     let output = beepBoop(input, name);
+    $(".resultBox").show();
     $("#output").text(output);
   })
 })
