e00e8a904b3c929adffd2cb0d832078a65c9705e
Chase Stokes <chasehunterstokes@gmail.com>
2022-01-21 09:49:27 -0800

    add js to account for third test

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,12 @@ function beepBoop(input) {
   let outputArray = [];
   for (i = 0; i <= input; i++) {
     let iString = i.toString();
-    if (iString.includes("1")) {
-      outputArray.push("beep")
+    if (iString.includes("2")) {
+      outputArray.push("boop");
+    } else if (iString.includes("1")) {
+      outputArray.push("beep");
     } else {
-      outputArray.push(i)
+      outputArray.push(i);
     }
   }
   return outputArray
