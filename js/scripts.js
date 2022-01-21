f21687650750d9f379e6d8ab71b337edbd8d1b3f
Chase Stokes <chasehunterstokes@gmail.com>
2022-01-21 10:25:23 -0800

    add js to account for fourth test

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,9 @@ function beepBoop(input) {
   let outputArray = [];
   for (i = 0; i <= input; i++) {
     let iString = i.toString();
-    if (iString.includes("2")) {
+    if (iString.includes("3")) {
+      outputArray.push("wont you be my neighbor")
+    } else if (iString.includes("2")) {
       outputArray.push("boop");
     } else if (iString.includes("1")) {
       outputArray.push("beep");
