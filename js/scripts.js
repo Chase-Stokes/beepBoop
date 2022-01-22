86be8c1458db94e92f27c1eebfd2784943eeec21
Chase Stokes <chasehunterstokes@gmail.com>
2022-01-21 16:08:42 -0800

    finish styling, pass last test, and clear the field when you resubmit with new inputs

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,10 @@ function beepBoop(input, name) {
   let outputArray = [];
   for (i = 0; i <= input; i++) {
     let iString = i.toString();
-    if (iString.includes("3")) {
-      outputArray.push("" +name + ", wont you be my neighbor ")
+    if (iString.includes("69") || iString.includes("420") ) {
+      outputArray.push( " haha funny number")
+    }   else if (iString.includes("3")) {
+        outputArray.push("" +name + ", wont you be my neighbor ")
     } else if (iString.includes(" 2")) {
       outputArray.push(" boop ");
     } else if (iString.includes(" 1")) {
